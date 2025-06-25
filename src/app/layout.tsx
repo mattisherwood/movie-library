86b20bea318d1df168e06393a5818008782d5ce5
Matt Isherwood <mugwumpman@gmail.com>
2025-06-25 17:15:37 +0100

    fix: remove console errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,17 +30,17 @@ export default function RootLayout({
         <link
           rel='icon'
           type='image/png'
-          href='/favicons/favicon-96x96.png'
+          href='/favicon-96x96.png'
           sizes='96x96'
         />
-        <link rel='icon' type='image/svg+xml' href='/favicons/favicon.svg' />
-        <link rel='shortcut icon' href='/favicons/favicon.ico' />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link rel='shortcut icon' href='/favicon.ico' />
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/favicons/apple-touch-icon.png'
+          href='/apple-touch-icon.png'
         />
-        <link rel='manifest' href='/favicons/site.webmanifest' />
+        <link rel='manifest' href='/manifest.json' />
       </head>
       <body className={notoSans.variable}>
         <Header />
