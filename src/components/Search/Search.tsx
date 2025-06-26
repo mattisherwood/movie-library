d90a8dff233def7f4ac2f4c1b48080dc20e14e20
Matt Isherwood <mugwumpman@gmail.com>
2025-06-26 15:30:31 +0100

    refactor(Search): add a debounce of 300ms

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx"
+import { useEffect, useState } from "react"
 import classes from "./Search.module.css"
 
 type Props = {
@@ -6,21 +7,34 @@ type Props = {
   setSearch: (search: string) => void
 }
 
-export const Search = ({ search, setSearch }: Props) => (
-  <div className={classes.root}>
-    <input
-      type='text'
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      placeholder='Search...'
-      className={classes.input}
-    />
-    {search.length > 0 && (
-      <button
-        aria-label='clear'
-        className={clsx(classes.clear, "icon-cross")}
-        onClick={() => setSearch("")}
+export const Search = ({ search, setSearch }: Props) => {
+  const [inputValue, setInputValue] = useState(search)
+
+  useEffect(() => {
+    const handler = setTimeout(() => setSearch(inputValue), 300)
+    return () => clearTimeout(handler)
+  }, [inputValue, setSearch])
+
+  useEffect(() => {
+    setInputValue(search)
+  }, [search])
+
+  return (
+    <div className={classes.root}>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder='Search...'
+        className={classes.input}
       />
-    )}
-  </div>
-)
+      {inputValue.length > 0 && (
+        <button
+          aria-label='clear'
+          className={clsx(classes.clear, "icon-cross")}
+          onClick={() => setInputValue("")}
+        />
+      )}
+    </div>
+  )
+}
