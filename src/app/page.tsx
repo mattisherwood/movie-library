c402d39a56c3584e6c588737d360042d9c9f6b21
Matt Isherwood <mugwumpman@gmail.com>
2025-06-26 16:18:22 +0100

    fix: wrap MovieFinder in Suspense
    This is required by NextJS to properly use useSearchParams()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { MovieFinder } from "@/components/MovieFinder/MovieFinder"
 import { RecentlyRated } from "@/components/RecentlyRated/RecentlyRated"
+import { Suspense } from "react"
 
 export default function Home() {
   return (
     <main>
-      <MovieFinder />
+      <Suspense fallback={<div>Loading...</div>}>
+        <MovieFinder />
+      </Suspense>
       <RecentlyRated />
     </main>
   )
